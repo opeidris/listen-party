f1c0a82571923292f8877a14f35e257fa8e38a3b
opeidris <opeyemi@opeidris.com>
2021-02-05 22:04:52 +0800

    Nuxt app with tailwind css

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://rsms.me/inter/inter.css'
+      }
+    ]
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: 'Inter var', sans-serif;
+}
+body {
+  @apply subpixel-antialiased;
+}
+</style>
